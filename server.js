abfd2d327e5010b7d0ecd5edae68e4f2f7add4e4
DeviSuli <suli.ajibola.dev@gmail.com>
2023-02-23 14:06:34 -0600

    server commit
    commit with server and initial view module

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require("path");
 
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
