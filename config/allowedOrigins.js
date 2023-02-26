c2d74e4b5e6ab5ca93b4149c66fbbcbf32cc0da1
DeviSuli <suli.ajibola.dev@gmail.com>
2023-02-26 15:25:27 -0600

    models commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "http://localHost:3000",
+  "https://www.dandrepairshop.com",
+  "https://dandrepairshop.com",
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions;");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 
+console.log(process.env.NODE_ENV);
+
+connectDB();
+
+app.use(cors(corsOptions));
 app.use(logger);
-app.use(express.json);
+app.use(express.json());
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/root"));
@@ -22,4 +34,17 @@ app.all("*", (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t ${err.syscall}\t${err.hostname}`,
+    "mongoErrorLog.log"
+  );
+});
