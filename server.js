05bf14512b842f860c268a8b21fefcd61922ac70
DeviSuli <suli.ajibola.dev@gmail.com>
2023-02-25 22:41:22 -0600

    logger and middleware commit

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require("path");
+const { logger } = require("./middleware/logger");
 
+app.use(logger);
+app.use(express.json);
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
   res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("text").send("404 not found");
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
