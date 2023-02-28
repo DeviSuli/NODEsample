0a27539e5ecaaf3e9b9915c5c65c658c5669ccfd
DeviSuli <suli.ajibola.dev@gmail.com>
2023-02-28 14:30:13 -0600

    controller updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,35 @@ const getAllUsers = asyncHandler(async (req, res) => {
 // @desc create new users
 // @route POST /users
 // @access Private
-const createNewUser = asyncHandler(async (req, res) => {});
+const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body;
+
+  //confirm data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
+
+  //check for DUplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
+
+  if (duplicate) {
+    return res.status(409).json({ message: "Duplicate Username!" });
+  }
+
+  //Hash Password
+  const hashedPwd = await bcrypt.hash(password, 10); // salt password
+
+  const userObject = { username, password: hashedPwd, roles };
+
+  //Create and store user
+  const user = await User.create(userObject);
+
+  if (user) {
+    res.status(201).json({ mesage: `New User ${username} created` });
+  } else {
+    res.status(401).json({ message: "Invalid user data received" });
+  }
+});
 
 // @desc Update a users
 // @route PATCH /users
@@ -29,7 +57,7 @@ const updateUser = asyncHandler(async (req, res) => {});
 // @access Private
 const deleteUser = asyncHandler(async (req, res) => {});
 
-module.expots = {
+module.exports = {
   getAllUsers,
   createNewUser,
   updateUser,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/", userController.getAllUsers);
-router.post("/", userController.createNewUser);
-router.patch("/", userController.updateUser);
-router.delete("/", userController.deleteUser);
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createNewUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
