7a25da1c1c524559a4e330aef0e3e6650d29620c
DeviSuli <suli.ajibola.dev@gmail.com>
2023-03-04 23:59:22 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
