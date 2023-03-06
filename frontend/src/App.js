6ea40d3669b29b7ceb861098da34c1547dec5899
DeviSuli <suli.ajibola.dev@gmail.com>
2023-03-05 23:04:28 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-
+import { Routes, Route } from "react-router-dom";
+import Layouts from "./components/Layouts";
 
 function App() {
   return (
-
+    <Routes>
+      <Route path="/" element={<Layouts />}></Route>
+    </Routes>
   );
 }
 
