151490be5ddcc606ebb02c57a7aa6e286c84c8bc
DeviSuli <suli.ajibola.dev@gmail.com>
2023-03-09 19:23:47 -0600

    Update DashLayout.js

--- frontend/src/components/DashLayout.js
+++ frontend/src/components/DashLayout.js
@@ -3,7 +3,9 @@ import { Outlet } from "react-router-dom";
 const DashLayout = () => {
   return (
     <>
-      <div className="dash-container"></div>
+      <div className="dash-container">
+        <Outlet />
+      </div>
     </>
   );
 };
