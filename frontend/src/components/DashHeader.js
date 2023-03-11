429050e9d9378d7fc7e5ea3f9ebe2b8326db8da4
DeviSuli <suli.ajibola.dev@gmail.com>
2023-03-10 22:16:06 -0600

    Update DashHeader.js

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 const DashHeader = () => {
   const content = (
     <header className="dash-header">
-      <div className="dash-header__constainer"></div>
+      <div className="dash-header__constainer">
+        <Link to="/dash/notes">
+          <h1 className="dash-header__title">TechNotes</h1>
+        </Link>
+      </div>
     </header>
   );
 
