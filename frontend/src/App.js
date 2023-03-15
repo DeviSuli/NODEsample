1114a0a774bd59af05a6d1e98eae071eeb5c4adb
DeviSuli <suli.ajibola.dev@gmail.com>
2023-03-14 22:31:10 -0500

    Update App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Layouts from "./components/Layouts";
+import Public from "./components/Public";
+import Login from "./components/Login";
+import DashLayout from "./components/DashLayout";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layouts />}>
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dash" element={<DashLayout />}></Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
